c792185de670cb4fc131c45b601c471ea0f245eb
frzyc <frzyc@users.noreply.github.com>
2024-06-06 14:40:21 -0400

    Initial commit

---
+++ apps/nx_import_lib_json-e2e/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx_import_lib_json-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/nx_import_lib_json-e2e',
+};
---
+++ apps/nx_import_lib_json/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nx_import_lib_json',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nx_import_lib_json',
+};
